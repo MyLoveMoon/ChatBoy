52a295e8267c6c687bf9ca5727a52446c7abdde3
ᴍʏʟᴏᴠᴇᴍᴏᴏɴ <90930470+MyLoveMoon@users.noreply.github.com>
2024-04-17 22:30:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 API_ID :int = int(os.environ.get("API_ID", None))
 API_HASH :str = os.environ.get("API_HASH", "")
 BOT_TOKEN :str= os.environ.get("BOT_TOKEN", "")
-UPDATE_CHNL :str = os.environ.get("UPDATE_CHNL", "mr_sukkun")
-SUPPORT_GRP :str = os.environ.get("SUPPORT_GRP", "the_support_chat")
+UPDATE_CHNL :str = os.environ.get("UPDATE_CHNL", "DarkLightFederation")
+SUPPORT_GRP :str = os.environ.get("SUPPORT_GRP", "DarkLightFederation")
 START_IMG :str = os.environ.get(
-    "START_IMG", "https://graph.org/file/d4412c7b411ca8da9e177.jpg"
+    "START_IMG", "https://graph.org/file/9343555f5c4f395d86533.jpg"
 )
 
 MONGO_DB_URI :str = os.environ.get(
     "MONGO_DB_URI",
     "",
 )
-OWNER_ID :int= os.environ.get("OWNER_ID", 6728038801)
+OWNER_ID :int= os.environ.get("OWNER_ID", 5056205033)
 
